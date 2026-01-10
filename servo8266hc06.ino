3305148acc9c7cd81f108c1659861cd664e37b0d
shantanu <i.am.shantanu07@gmail.com>
2026-01-11 00:01:50 +0530

    Add servo control functionality with serial commands

---
+++ servo8266hc06.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+
+Servo s;
+
+int center = 90;
+int onPos  = 173;
+int offPos = 7
+;
+
+void setup() {
+  Serial.begin(9600);
+  s.attach(14);      // D5 = GPIO14
+  s.write(center);
+}
+
+void loop() {
+  if (Serial.available()) {
+    String cmd = Serial.readString();
+    cmd.trim();
+    cmd.toUpperCase();    // <<< this line makes it case-insensitive
+
+    if (cmd == "OFF") {
+      s.write(onPos);
+      delay(400);
+      s.write(center);
+      Serial.println("OFF done");
+    }
+
+    if (cmd == "ON") {
+      s.write(offPos);
+      delay(400);
+      s.write(center);
+      Serial.println("ON done");
+    }
+  }
+}
